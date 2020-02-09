436c9c035d731988fdf5f01bc34d511ef5dbc1b6
Mallikarjun Sai <mallikarjunsai@gmail.com>
2020-02-08 23:14:12 -0800

    IPL Match Analysis App update

--- src/main/scala/org/training/spark/practice/assignmentone/AverageFriendsPerAge.scala
+++ src/main/scala/org/training/spark/practice/assignmentone/AverageFriendsPerAge.scala
@@ -1,5 +1,21 @@
 package org.training.spark.practice.assignmentone
 
-object AverageFriendsPerAge {
+import org.apache.spark.{SparkConf, SparkContext}
 
+/*calculate the average number of friends based on their age.*/
+object AverageFriendsPerAge {
+  def main(args: Array[String]): Unit = {
+    val sparkCnf = new SparkConf().setMaster("local").setAppName(getClass.getName)
+    val sprkCxt = new SparkContext(sparkCnf)
+    val socialFrndsRDD = sprkCxt.textFile("src/main/resources/assignmentone/social_friends.csv")
+    val ageFrndsRDD = socialFrndsRDD.map(each => {
+      val splits = each.split(",")
+      // age      //frndsCnt
+      (splits(2),splits(3).toInt)
+    })
+    val ageFrndsCntRDD=ageFrndsRDD.aggregateByKey((0,0))((accr,each)=>(1+accr._1,accr._2+each),((accr,each)=>(accr._1+each._1,accr._2+each._2)))
+    ageFrndsCntRDD.collect().foreach(result=> println("Age- "+result._1 +" ,Avg no.of frnds: "+(result._2._2)/result._2._1))
+
+    Thread.sleep(300000)
+  }
 }
--- src/main/scala/org/training/spark/practice/assignmentone/IPLMatchsAnalysis.scala
+++ src/main/scala/org/training/spark/practice/assignmentone/IPLMatchsAnalysis.scala
@@ -1,5 +1,46 @@
 package org.training.spark.practice.assignmentone
 
+import org.apache.spark.{SparkConf, SparkContext}
+/*IPL Match Analysis - Best First Bat win stadium & percentage , Best First Bowl Win Stadium & Percentage*/
 object IPLMatchsAnalysis {
+  def main(args: Array[String]): Unit = {
+    val spCf = new SparkConf().setMaster("local").setAppName(getClass.getName)
+    val spCxt = new SparkContext(spCf)
+    val iplMatchsRDD = spCxt.textFile("src/main/resources/assignmentone/matches.csv")
+    val mAlysDataRDD = iplMatchsRDD.map(IPLMatchUtil.extractMatchDetails(_))
+    val venuRunWicketRDD = mAlysDataRDD.map(matchDetails => (matchDetails.venue, (matchDetails.wonByRuns, matchDetails.wonByWkts)))
+    //val aggrResultRDD=venuRunWicketRDD.reduceByKey((accr,each)=>(accr._1+each._1,accr._2+each._2))
+    val aggrResultRDD = venuRunWicketRDD.reduceByKey((accr, each) => (if (each._1 > 0) {
+      accr._1 + each._1
+    } else {
+      accr._1
+    }, if (each._2 > 0) {
+      accr._2 + each._2
+    } else {
+      accr._2
+    }))
+    val batWinPrctRDD = aggrResultRDD.map(each => (each._1, (each._2._1 * 100) / (each._2._1 + each._2._2)))
+    val batWinTake=batWinPrctRDD.collect().sortBy(_._2).reverse.take(1);
+    println("Batting Win % ")
+    println(batWinTake.foreach(println))
+    val fieldWinPrctRDD = aggrResultRDD.map(each => (each._1, (each._2._2 * 100) / (each._2._1 + each._2._2)))
+    val fieldWinTake=fieldWinPrctRDD.collect().sortBy(_._2).reverse.take(1);
+    println("Bowling Win % :")
+    println(fieldWinTake.foreach(println))
 
+    Thread.sleep(300000)
+
+  }
+}
+
+case class MatchDetails(tossDecision: String, wonByRuns: Int, wonByWkts: Int, venue: String)
+
+
+object IPLMatchUtil {
+
+  def extractMatchDetails(data: String): MatchDetails = {
+    val splits = data.split(",")
+    val matchDetails = new MatchDetails(splits(7), splits(11).toInt, splits(12).toInt, splits(14))
+    matchDetails
+  }
 }
